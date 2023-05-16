112acde9987a95cbc315a09435304b3450162338
Jakub Łaba <77542257+jakublaba@users.noreply.github.com>
2023-05-16 23:03:52 +0200

    Wip/#5 ip hash strategy (#13)
    * Add IP hash implementation
    
    * Refactor existing strategies to include struct RequestContext
    
    ---------
    
    Co-authored-by: Maciej Michalski <legeof008@gmail.com>
    Co-authored-by: michalskim <72550341+legeof008@users.noreply.github.com>

--- src/load_balancing/random.rs
+++ src/load_balancing/random.rs
@@ -1,24 +1,23 @@
 use rand::Rng;
 
+use crate::load_balancing::RequestContext;
 use async_trait::async_trait;
 
 use crate::load_balancing::strategy::Strategy;
 
-pub(crate) struct RandomStrategy {
-    pub(crate) receiver_addresses: Vec<String>,
-}
+pub(crate) struct RandomStrategy;
 
 impl RandomStrategy {
-    pub fn new(receiver_addresses: Vec<String>) -> Self {
-        RandomStrategy { receiver_addresses }
+    pub fn new() -> Self {
+        RandomStrategy {}
     }
 }
 
 #[async_trait]
 impl Strategy for RandomStrategy {
-    fn choose_receiver_ip(&self) -> String {
-        let idx = rand::thread_rng().gen_range(0..self.receiver_addresses.len());
-        self.receiver_addresses[idx].clone()
+    fn choose_receiver_ip<'l>(&self, ctx: &'l RequestContext) -> String {
+        let idx = rand::thread_rng().gen_range(0..ctx.receiver_ips.len());
+        ctx.receiver_ips[idx].clone()
     }
 }
 
@@ -26,18 +25,23 @@ impl Strategy for RandomStrategy {
 mod test {
     use crate::load_balancing::random::RandomStrategy;
     use crate::load_balancing::strategy::Strategy;
+    use crate::load_balancing::RequestContext;
 
     #[test]
     fn should_generate_numbers_in_range() {
-        let rand_strategy = RandomStrategy::new(vec![
-            String::from("127.0.0.1:81"),
-            String::from("127.0.0.1:82"),
-            String::from("127.0.0.1:83"),
-        ]);
+        let rand_strategy = RandomStrategy::new();
+        let ctx = RequestContext {
+            requester_ip: String::from("1.2.3.4:5"),
+            receiver_ips: &vec![
+                String::from("127.0.0.1:81"),
+                String::from("127.0.0.1:82"),
+                String::from("127.0.0.1:83"),
+            ],
+        };
 
         for _i in 0..100 {
-            let ip = rand_strategy.choose_receiver_ip();
-            assert!(rand_strategy.receiver_addresses.contains(&ip));
+            let ip = rand_strategy.choose_receiver_ip(&ctx);
+            assert!(ctx.receiver_ips.contains(&ip));
         }
     }
 }
