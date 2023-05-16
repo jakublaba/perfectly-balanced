a680c19392b707fa63aea524a4124e6d2e513592
Maciej Michalski <legeof008@gmail.com>
2023-05-16 22:55:27 +0200

    Add IP hash implementation

--- src/load_balancing/ip_hash.rs
+++ src/load_balancing/ip_hash.rs
@@ -1,17 +1,108 @@
 use crate::load_balancing::strategy::Strategy;
+use crate::load_balancing::RequestContext;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+use async_trait::async_trait;
 
 pub(crate) struct IpHashStrategy {
     pub(crate) receiver_addresses: Vec<String>,
 }
 
 impl IpHashStrategy {
-    pub fn new(receiver_addresses: Vec<String>) -> Self {
-        IpHashStrategy { receiver_addresses }
+    pub fn new() -> Self {
+        IpHashStrategy {}
+    }
+
+     fn calculate_hash<T: Hash>(&self, t: &T) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        t.hash(&mut hasher);
+        hasher.finish()
+    }
+
+     fn hash_to_idx(&self, hash: u64, receiver_vector_len: usize) -> usize {
+         hash as usize % receiver_vector_len
     }
 }
 
+#[async_trait]
 impl Strategy for IpHashStrategy {
-    fn choose_receiver_ip(&self) -> String {
-        String::from("ip hash")
+    fn choose_receiver_ip<'l>(&self, ctx: &'l RequestContext) -> String {
+        let requester_hash = self.calculate_hash(&ctx.requester_ip);
+        let idx = self.hash_to_idx(requester_hash,ctx.receiver_ips.len());
+        ctx.receiver_ips[idx].clone()
+    }
+
+}
+
+#[cfg(test)]
+mod test {
+    use crate::load_balancing::ip_hash::IpHashStrategy;
+    use crate::load_balancing::RequestContext;
+    use crate::load_balancing::strategy::Strategy;
+
+    #[test]
+    fn should_generate_different_hashes() {
+        let ip_hash_strategy = IpHashStrategy::new();
+        let receiver_addresses = &vec![
+           String::from("127.0.0.1:81"),
+           String::from("127.0.0.1:82"),
+           String::from("127.0.0.1:83")
+        ];
+        let mut hash_vector = Vec::new();
+
+        for i in receiver_addresses {
+            hash_vector.push(ip_hash_strategy.calculate_hash(i));
+        }
+        assert_ne!(hash_vector[0],hash_vector[1]);
+        assert_ne!(hash_vector[0],hash_vector[2]);
+        assert_ne!(hash_vector[1],hash_vector[2]);
+    }
+    #[test]
+    fn should_map_to_index_within_bounds() {
+        let ip_hash_strategy = IpHashStrategy::new();
+        let requester_hash = ip_hash_strategy.calculate_hash(&String::from("127.0.0.1:82"));
+        let idx = ip_hash_strategy.hash_to_idx(requester_hash,5);
+
+        assert!(requester_hash > 5);
+        assert!(idx < 5);
+    }
+
+    #[test]
+    fn should_choose_same_ip_for_same_client_and_different_for_different() {
+        let ip_hash_strategy = IpHashStrategy::new();
+        let ctx = RequestContext {
+            requester_ip: String::from("1.2.3.4:5"),
+            receiver_ips: &vec![
+                String::from("127.0.0.1:81"),
+                String::from("127.0.0.1:82"),
+                String::from("127.0.0.1:83"),
+            ],
+        };
+
+        let receiver_ip1 = ip_hash_strategy.choose_receiver_ip(&ctx);
+        let receiver_ip2 = ip_hash_strategy.choose_receiver_ip(&ctx);
+        assert_eq!(receiver_ip1, receiver_ip2);
+    }
+
+    #[test]
+    fn should_choose_different_ip_for_different_clients() {
+        let ip_hash_strategy = IpHashStrategy::new();
+        let ctx1 = RequestContext {
+            requester_ip: String::from("1.2.3.4:5"),
+            receiver_ips: &vec![
+                String::from("127.0.0.1:81"),
+                String::from("127.0.0.1:82"),
+                String::from("127.0.0.1:83"),
+            ],
+        };
+        let ctx2 = RequestContext {
+            requester_ip: String::from("1.2.3.4:6"),
+            ..ctx1
+        };
+
+        let ip1 = ip_hash_strategy.choose_receiver_ip(&ctx1);
+        let ip2 = ip_hash_strategy.choose_receiver_ip(&ctx2);
+
+        assert_ne!(ip1, ip2);
     }
 }
--- src/load_balancing/mod.rs
+++ src/load_balancing/mod.rs
@@ -2,26 +2,34 @@ use crate::config::config_properties::ConfigProperties;
 use crate::load_balancing::ip_hash::IpHashStrategy;
 use crate::load_balancing::random::RandomStrategy;
 use crate::load_balancing::round_robin::RoundRobinStrategy;
-use crate::load_balancing::strategy::{Strategy, StrategyOption};
+use crate::load_balancing::strategy::StrategyOption::{IpHash, Random, RoundRobin};
+use crate::load_balancing::strategy::{Strategy};
 
+pub(crate) mod ip_hash;
 pub(crate) mod random;
 pub(crate) mod round_robin;
-pub(crate) mod ip_hash;
 pub(crate) mod strategy;
 
 pub(crate) struct LoadBalancer {
     address: String,
+    receiver_addresses: Vec<String>,
     strategy: Box<dyn Strategy>,
+}
+
+pub(crate) struct RequestContext<'l> {
+    pub(crate) requester_ip: String,
+    pub(crate) receiver_ips: &'l Vec<String>,
 }
 
 impl LoadBalancer {
     pub(crate) fn new(config: ConfigProperties) -> Self {
         LoadBalancer {
             address: config.address,
+            receiver_addresses: config.receiver_addresses,
             strategy: match config.strategy {
-                StrategyOption::Random => Box::new(RandomStrategy::new(config.receiver_addresses)),
-                StrategyOption::RoundRobin => Box::new(RoundRobinStrategy::new(config.receiver_addresses)),
-                StrategyOption::IpHash => Box::new(IpHashStrategy::new(config.receiver_addresses)),
+                Random => Box::new(RandomStrategy::new()),
+                RoundRobin => Box::new(RoundRobinStrategy::new()),
+                IpHash => Box::new(IpHashStrategy::new()),
             },
         }
     }
