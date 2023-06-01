fb43d83e411aa887b903b76fa1760bd67499a940
Maciej Michalski <legeof008@gmail.com>
2023-06-01 23:29:51 +0200

    chore: update skeleton of the main method to use asynchronous tcp port binds

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,23 @@ mod config;
 mod load_balancing;
 
 use log::Level;
+use tokio::net::TcpListener;
+use crate::config::configure;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     simple_logger::init_with_level(Level::Debug)?;
+    let global_configuration = configure("src/config/test_resources/all_props.json")?;
+    let listener = TcpListener::bind(global_configuration.address.clone()).await?;
+    log::debug!("Establishing a connection on {}", global_configuration.address.clone());
+    loop {
+        let (socket, _) = listener.accept().await?;
+        process_incoming_request(socket).await;
+    }
 
     Ok(())
+}
+
+async fn process_incoming_request<T>(socket: T) {
+    log::debug!("Connection has been established");
 }
