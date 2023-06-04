06e1d0a3e319b0e3d5d6f9499d4acf9f217081e5
Maciej Michalski <legeof008@gmail.com>
2023-06-04 19:29:06 +0200

    Add bidirectional TCP forwarding

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,37 @@
 mod config;
 mod load_balancing;
 
-use log::Level;
-use tokio::net::TcpListener;
 use crate::config::configure;
+use crate::load_balancing::LoadBalancer;
+use log::Level;
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     simple_logger::init_with_level(Level::Debug)?;
     let global_configuration = configure("src/config/test_resources/all_props.json")?;
-    let listener = TcpListener::bind(global_configuration.address.clone()).await?;
-    log::debug!("Establishing a connection on {}", global_configuration.address.clone());
-    loop {
-        let (socket, _) = listener.accept().await?;
-        process_incoming_request(socket).await;
+    {
+        let config = configure("src/config/test_resources/all_props.json")?;
+        log::info!("Establishing a connection on {}", config.address.clone());
     }
+    let listener = TcpListener::bind(global_configuration.address.clone()).await?;
+    let load_balancer = LoadBalancer::new(global_configuration);
 
+    while let Ok((mut ingress, addr)) = listener.accept().await {
+
+        let mut egress = TcpStream::connect(load_balancer.get_receiver_ip(addr.to_string())).await?;
+
+        match tokio::io::copy_bidirectional(&mut ingress, &mut egress).await {
+
+            Ok((to_egress, to_ingress)) => {
+                log::info!(
+                        "Connection ended gracefully ({to_egress} bytes from client, {to_ingress} bytes from server)"
+                    );
+            }
+            Err(err) => {
+                log::info!("Error while proxying: {}", err);
+            }
+        }
+    }
     Ok(())
-}
-
-async fn process_incoming_request<T>(socket: T) {
-    log::debug!("Connection has been established");
 }
